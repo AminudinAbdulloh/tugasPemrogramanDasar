682b4bcc554c106e0c119e588de0792373cf9520
Aminudin Abdulloh <120945945+AminudinAbdulloh@users.noreply.github.com>
2022-12-19 19:25:23 +0700

    Update WarungMakan.java

--- WarungMakan.java
+++ WarungMakan.java
@@ -23,56 +23,59 @@ public class WarungMakan {
 
 				
 				do {
-					System.out.print("Pilih [1-7] :");
+					System.out.print("Pilih [1-7] : ");
 					pilihan = input.nextInt();
 					
 				switch(pilihan) {
 				
 				case 1 :
-					System.out.print("Nasi Goreng, beli berapa porsi =");
+					System.out.print("Nasi Goreng, beli berapa porsi = ");
 					jumlah = input.nextInt();
 					total = jumlah*10000;
-					System.out.println("Nasi Goreng yang anda pesan seharga = Rp. " + total);
+					System.out.println("Nasi Goreng yang anda pesan seharga = Rp. " + total + "\n");
 					break;
 					
 				case 2 :
-					System.out.print("Nasi Uduk, beli berapa porsi =");
+					System.out.print("Nasi Uduk, beli berapa porsi = ");
 					jumlah = input.nextInt();
 					total = jumlah*8000;
-					System.out.println("Nasi Uduk yang anda pesan seharga = Rp. " + total);
+					System.out.println("Nasi Uduk yang anda pesan seharga = Rp. " + total + "\n");
 					break;
 					
 				case 3 :
-					System.out.print("Nasi Kuning, beli berapa porsi =");
+					System.out.print("Nasi Kuning, beli berapa porsi = ");
 					jumlah = input.nextInt();
 					total = jumlah*9500;
-					System.out.println("Nasi Kuning yang anda pesan seharga = Rp. " + total);
+					System.out.println("Nasi Kuning yang anda pesan seharga = Rp. " + total + "\n");
 					break;
 					
 				case 4 :
-					System.out.print("Nasi Kucing, beli berapa porsi =");
+					System.out.print("Nasi Kucing, beli berapa porsi = ");
 					jumlah = input.nextInt();
 					total = jumlah*2000;
-					System.out.println("Nasi Kucing yang anda pesan seharga = Rp. " + total);
+					System.out.println("Nasi Kucing yang anda pesan seharga = Rp. " + total + "\n");
 					break;
 					
 				case 5 :
-					System.out.print("Nasi Orak-Arik, beli berapa porsi =");
+					System.out.print("Nasi Orak-Arik, beli berapa porsi = ");
 					jumlah = input.nextInt();
 					total = jumlah*9000;
-					System.out.println("Nasi Orak-Arik yang anda pesan seharga = Rp. " + total);
+					System.out.println("Nasi Orak-Arik yang anda pesan seharga = Rp. " + total + "\n");
 					break;
 					
 				case 6 :
-					System.out.print("Nasi Magelangan, beli berapa porsi =");
+					System.out.print("Nasi Magelangan, beli berapa porsi = ");
 					jumlah = input.nextInt();
 					total = jumlah*12000;
-					System.out.println("Nasi Magelangan yang anda pesan seharga = Rp. " + total);
+					System.out.println("Nasi Magelangan yang anda pesan seharga = Rp. " + total + "\n");
 					break;
 					
 				case 7 :
 					System.out.print("Pesanan Selesai, Sistem Keluar");
 					System.exit(0);
+						
+				default :
+					System.out.print("Pilihan yang anda masukkan salah!!!");
 					
 				}
 				} while (pilihan < 7 && pilihan >= 1);
